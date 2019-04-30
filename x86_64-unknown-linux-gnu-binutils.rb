3db47974f0613d47bfc82a091a064b3c5d441a16
Kevin Boos <kevinaboos@gmail.com>
2019-04-30 16:14:39 -0700

    trying unknown-linux-gnu target

---
+++ x86_64-unknown-linux-gnu-binutils.rb
@@ -0,0 +1,25 @@
+require 'formula'
+
+class X8664UnknownLinuxGnuBinutils < Formula
+  homepage 'http://gcc.gnu.org'
+  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.gz'
+  sha256 '26253bf0f360ceeba1d9ab6965c57c6a48a01a8343382130d1ed47c468a3094f'
+
+  depends_on 'gcc@8' => :build
+  def install
+    ENV['CC'] = '/usr/local/opt/gcc@8/bin/gcc-8'
+    ENV['CXX'] = '/usr/local/opt/gcc@8/bin/g++-8'
+    ENV['CPP'] = '/usr/local/opt/gcc@8/bin/cpp-8'
+    ENV['LD'] = '/usr/local/opt/gcc@8/bin/gcc-8'
+
+    mkdir 'build' do
+      system '../configure', '--disable-nls', '--target=x86_64-unknown-linux-gnu','--disable-werror',
+                             '--enable-gold=yes',
+                             "--prefix=#{prefix}"
+      system 'make all'
+      system 'make install'
+
+    end
+  end
+
+end
---
+++ x86_64-unknown-linux-gnu-gcc.rb
@@ -0,0 +1,40 @@
+require 'formula'
+
+class X8664UnknownLinuxGnuGcc < Formula
+  homepage 'http://gcc.gnu.org'
+  url "http://mirror.tochlab.net/pub/gnu/gcc/gcc-8.3.0/gcc-8.3.0.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/gcc/gcc-8.3.0/gcc-8.3.0.tar.xz"
+  sha256 "64baadfe6cc0f4947a84cb12d7f0dfaf45bb58b7e92461639596c21e02d97d2c"
+
+  depends_on "gmp"
+  depends_on "libmpc"
+  depends_on "mpfr"
+  depends_on "isl"
+  depends_on 'x86_64-unknown-linux-gnu-binutils'
+
+  def install
+    binutils = Formulary.factory 'x86_64-unknown-linux-gnu-binutils'
+
+    ENV['CC'] = '/usr/local/opt/gcc@8/bin/gcc-8'
+    ENV['CXX'] = '/usr/local/opt/gcc@8/bin/g++-8'
+    ENV['CPP'] = '/usr/local/opt/gcc@8/bin/cpp-8'
+    ENV['LD'] = '/usr/local/opt/gcc@8/bin/gcc-8'
+    ENV['PATH'] += ":#{binutils.prefix/"bin"}"
+
+    mkdir 'build' do
+      system '../configure', '--disable-nls', '--target=x86_64-unknown-linux-gnu','--disable-werror',
+                             "--prefix=#{prefix}",
+                             "--enable-languages=c,c++,go",
+                             "--without-headers",
+                             "--with-gmp=#{Formula["gmp"].opt_prefix}",
+                             "--with-mpfr=#{Formula["mpfr"].opt_prefix}",
+                             "--with-mpc=#{Formula["libmpc"].opt_prefix}"
+      system 'make all-gcc'
+      system 'make install-gcc'
+      FileUtils.ln_sf binutils.prefix/"x86_64-unknown-linux-gnu", prefix/"x86_64-unknown-linux-gnu"
+      system 'make all-target-libgcc'
+      system 'make install-target-libgcc'
+      FileUtils.rm_rf share/"man"/"man7"
+    end
+  end
+end
